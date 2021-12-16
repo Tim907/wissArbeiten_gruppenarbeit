f1dcbbc3dea51b2d17822f1aff5079eef12f5489
smtinova <tim.novak@tu-dortmund.de>
2021-12-16 13:26:04 +0100

    Create erzeuge_daten.R
    leeres R-Skript für Aufgabe 1 mit grober Aufgabenliste erstellt.

---
+++ erzeuge_daten.R
@@ -0,0 +1,17 @@
+
+# Dieses R-Skript dient dem Simulieren eines Datensatzes nach Aufgabe 1.
+# Er soll 100 Beobachtungen auf 5 Variablen beinhalten, exclusive der ersten Spalte ID.
+
+# todo dataFrame mit 6 Spalten erzeugen
+
+# todo (1)
+
+# todo (2)
+
+# todo (3)
+
+# todo (4)
+
+# todo (5)
+
+# todo 2. \\ Datensatz als .csv abspeichern

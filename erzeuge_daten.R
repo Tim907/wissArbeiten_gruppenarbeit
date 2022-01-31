dddf6eaced7ca53237f378d1d396d50bd7f9697c
CynthiaN01 <cynthia.njontie@tu-dortmund.de>
2022-01-31 19:04:22 +0100

    Teil 3 und Umstrukturierungen
    Ergänzung von Teil 3 (& 4) und ein paar Umstrukturierungen nach Absprache im Code

--- erzeuge_daten.R
+++ erzeuge_daten.R
@@ -5,7 +5,7 @@
 
 # todo dataFrame mit 6 Spalten erzeugen
 N = 100
-data = data.frame(ID = 1:N,
+person = data.frame(ID = 1:N,
                   "Alter" = numeric(N),
                   "Studienfach" = character(N),
                   "Interesse an Mathematik" = numeric(N), # auch factor moeglich
@@ -14,46 +14,76 @@ data = data.frame(ID = 1:N,
 #Replizierbarkeit
 #set.seed()
 
+
 # todo (1)
-col = rnorm(N, 25, 2)
-#data = cbind(data, Alter = col)
-data$Alter = col
+set.seed(11)
+person$Alter = rnorm(N, 25, 2)
+
 
 # todo (2)
-auswahl = c("Statistik", "Data Science", "Mathe", "Informatik")
-# Willkuerliches Festlegen der wahren Wahrscheinlichkeiten:
-p_statistik = 3.5 - 2
-# 3.5 entspricht dem Erwartungswert eines Wuerfelwurfs
-# 3.5 - 2 = 1.5
+auswahl = c("Statistik", "Data Science", "Informatik", "Mathe")
 
-p_informatik = pi - 2
-# ungefaehr 3.14 - 2 = 1.14
+# Fachwahl simuleirt die Fachwahl von n Studenten in einem Vektor mit 4 
+# verschiedenen Faechern und willkuerlichen, aber festen relativen Haeufigkeiten
+#
+Fachwahl = function(faecher, n){
+  stopifnot(length(faecher) == 4,
+            is.numeric(n))
+  
+  # Willkuerliches Festlegen der wahren Wahrscheinlichkeiten:
+  
+  p_F1.2 = 3.5 - 2
+  # 3.5 entspricht dem Erwartungswert eines Wuerfelwurfs
+  # 3.5 - 2 = 1.5
+  
+  p_F3 = pi - 2
+  # ungefaehr 3.14 - 2 = 1.14
+  
+  p_F4 = exp(1) - 2
+  # ungefaehr 2.72 - 2 = 0.72
+  
+  # Wahrscheinlichkeiten zwischen 0 und 1 normieren:
+  total = 2 * p_F1.2 + p_F3 + p_F4
+  p_F1.2 = p_F1.2 / total
+  p_F3 = p_F3 / total
+  p_F4 = p_F4 / total
+  
+  Wahl = sample(faecher, n, replace = TRUE, c(p_F1.2, p_F1.2, p_F3, p_F4))
+  
+  return(Wahl)
+}
 
-p_mathe = exp(1) - 2
-# ungefaehr 2.72 - 2 = 0.72
+set.seed(22)
+person$Studienfach = Fachwahl(auswahl, N)
+#table(person$Studienfach)
 
-# Wahrscheinlichkeiten zwischen 0 und 1 normieren:
-total = 2 * p_statistik + p_mathe + p_informatik
-p_statistik = p_statistik / total
-p_mathe = p_mathe / total
-p_informatik = p_informatik / total
-
-col = sample(auswahl, N, replace = TRUE, 
-             c(p_statistik, p_statistik, p_mathe, p_informatik))
-#table(col)
-#data = cbind(data, Studienfach = col)
-data$Studienfach = col
 
 # todo (3)
+M_Interesse = as.numeric(as.vector(factor(person$Studienfach, 
+                            labels = c(0.3, 0.4, 0.6, 0.85), 
+                            levels = c("Informatik", "Data Science", "Statistik", 
+                                       "Mathe"))))
+set.seed(33)
+person$Interesse.an.Mathematik = sapply(M_Interesse, rbinom, n = 1, size = 6) + 1
+#split(person$Interesse.an.Mathematik, person$Studienfach)
 
-#data = cbind(data, col)
 
 # todo (4)
+P_Interesse = 
+#  as.numeric(as.vector(factor(person$Studienfach, 
+#                              labels = c(0.25, 0.42, 0.7, 0.9),
+#                              levels = c("Mathe", "Statistik", "Data Science", 
+#                                         "Informatik"))))
+  #oder
+#  sample(1:7, N, replace = TRUE)
+  
+set.seed(44)
+#person$Interesse.an.Programmieren = P_Interesse
 
-#data = cbind(data, col)
 
 # todo (5)
 
 #data = cbind(data, col)
+
 
 # todo 2. \\ Datensatz als .csv abspeichern

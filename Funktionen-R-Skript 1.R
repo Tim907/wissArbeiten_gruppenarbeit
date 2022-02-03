8c61423e1921cbfcccce7153af4cdad1dfe1056f
smtinova <tim.novak@tu-dortmund.de>
2022-02-03 16:45:29 +0100

    Funktionsskript Dateien erstellt
    Gerüst für die Funktionsskripte gebaut

---
+++ Funktionen-R-Skript 1.R
@@ -0,0 +1,17 @@
+
+# Bearbeitung von Aufgabe 3
+
+# Falls Probleme beim Laden, probiert: Session/Set Working Directory/To Source File Location
+source("Funktionen-R-Skript 2.R")
+
+# todo (a)
+
+# todo (b)
+
+# todo (c)
+
+# todo (d)
+
+# todo (e)
+
+# todo (f)
---
+++ Funktionen-R-Skript 2.R
@@ -0,0 +1,2 @@
+# Hier Helferfunktionen implementieren, welche nur in
+# Funktionen-R-Skript 1 benutzt werden duerfen.

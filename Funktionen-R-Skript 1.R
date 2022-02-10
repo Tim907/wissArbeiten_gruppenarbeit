a6714b33d557a54d753e644d089525269f44487c
Hanjin99 <qq784365536@gmail.com>
2022-02-10 18:02:55 +0100

    Aufgabe a und b
    Aufgabe a ist soweit fertig, Aufgabe b fehlen noch die Implementierung für qualitativ() und ordinal().

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -6,7 +6,40 @@ source("Funktionen-R-Skript 2.R")
 
 # todo (a)
 
+metrischeVariablen = function(a){
+  res = summary(a)
+  
+  minimum = as.double(res[1])
+  maximum = as.double(res[6])
+  quatil1 = as.double(res[3])
+  quatil3 = as.double(res[5])
+  median = as.double(res[3])
+  mean = as.double(res[4])
+  empirischeVarianz = var(a)
+  
+  res = list(Min = minimum,
+             Max = maximum,
+             Quatil.1 = quatil1,
+             Quatil.3 = quatil3,
+             Median = median,
+             Mean = mean,
+             Var = empirischeVarianz)
+  
+  return(res)
+}
+
 # todo (b)
+
+kategorialeVariablen = function(a, Merkmal){
+  stopifnot(Merkmal == "ordinal" | Merkmal == "qualitativ")
+  
+  if(Merkmal == "ordinal"){
+    return(ordinal(a))
+  }
+  else{
+    return(qualitativ(a))
+  }
+}
 
 # todo (c)
 
--- Funktionen-R-Skript 2.R
+++ Funktionen-R-Skript 2.R
@@ -1,2 +1,16 @@
 # Hier Helferfunktionen implementieren, welche nur in
 # Funktionen-R-Skript 1 benutzt werden duerfen.
+
+#Helferfunktion fuer b
+
+#qualitativ(): eine Funktion, die die qualitativen Merkmale bearbeitet
+
+qualitativ = function(a){
+  
+}
+
+#ordinal(): eine Funktion, die die ordinalen Merkmale bearbeitet
+
+ordinal = function(a){
+  
+}

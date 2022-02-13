6236e3f151b70fd248ea5ae2055e79df1995daa6
smtinova <tim.novak@tu-dortmund.de>
2022-02-13 17:33:01 +0100

    Update Funktionen-R-Skript 1.R
    Teilaufgabe 3d) implementiert

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -12,6 +12,25 @@ source("Funktionen-R-Skript 2.R")
 
 # todo (d)
 
+# cor_metric_dicho liefert mittels logistischer Regression deskriptive Statistiken 
+# ueber den Zusammenhang zwischen einer metrischen und einer bivariaten Variable.
+# Input:
+#   x, numerischer Vektor der Eingabe-Variable
+#   y, numerischer Vektor der Ziel-Variable
+# Output:
+#   prediction, die Vorhersage fuer alle x-Werte des berechneten Modells.
+#   summary, das summary-Objekt des Modells zur Beurteilung der Guete.
+cor_metric_dicho = function(x, y) {
+  
+  model = glm(y ~ x, family=binomial(link = "logit"))
+  predictions = model1$fitted.values
+  info = summary(model)
+  return(list(prediction = predictions, summary = summary(model)))
+}
+# data = read.csv2("person.csv")
+# correlation = cor_metric_dicho(data$Interesse.an.Mathematik,
+#   ifelse(data$Mathe.LK == "ja", 1, 0))
+
 # todo (e)
 
 # todo (f)

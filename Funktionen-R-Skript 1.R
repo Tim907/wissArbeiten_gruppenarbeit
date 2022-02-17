6d41b9d9753f750215184118fb383ff15df5d319
CynthiaN01 <cynthia.njontie@tu-dortmund.de>
2022-02-17 21:16:40 +0100

    Aufgabe 3c)
    code für Aufgabe 3 c) und für seine Helferfunktion fertig.

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -10,8 +10,79 @@ source("Funktionen-R-Skript 2.R")
 
 # todo (c)
 
+# relate_categorial
+#
+# Funktion: beschreibt den Zusammenhang zwischen zwei kategorialen Variablen mit
+#           einer Kontingenztafel, den bedingten Verteilungen und Cramers und 
+#           Pearsons korrigiertes Kontingenzmass.
+#
+# input:  X       - Vektor mit den Merkmalsauspraegungen eines kategorialen 
+#                   Merkmals
+#         Y       - Vektor mit den Merkmalsauspraegungen eines kategorialen
+#                   Merkmals, selbe Laenge wie X
+#         Merkmal - Vektor Laenge 2; jeweils entweder "nominal" oder "ordinal", 
+#                   "ordinal", wenn Mermal eine Ordnung hat
+#         absolut - logisch; gibt an ob die absoluten Haeufigkeiten gegeben 
+#                   werden sollen
+#         dnames  - Vektor Laenge 2; die Namen der beiden Variablen. Wenn ein 
+#                   Name unbekannt ist, dann NA angeben.
+#
+# output: Benannte Liste mit
+#           - Kontingenztafel/Korrelationstabelle
+#           - Tabelle der bedingten Haeufigkeitsverteilungen h(X|Y) und h(Y|X)
+#           - 2 normierte Kontingenzmasse
+#           - maximale und minimale Auspraegung der Kontingenzmasse.
+#
+relate_categorial = function( X, Y, Merkmal = c("nominal", "nominal"), 
+                              absolut = FALSE, dnames = c(NULL, NULL) ){
+  stopifnot(length(X) == length(Y),
+            Merkmal %in% c("nominal", "ordinal"),
+            length(Merkmal) == 2,
+            length(dnames) <= 2)
+  
+  n = length(X)
+  normer = ifelse(absolut, 1, n)
+  
+  if(is.null(dnames))dnames = c("X", "Y")
+  if(length(dnames == 1)){
+    length(dnames) = 2
+    dnames[which(is.na(dnames))] = c("X", "Y")[which(is.na(dnames))]
+  }
+  if(dnames[1] != "X")dnames[1] = paste("X:", dnames[1])
+  if(dnames[2] != "Y")dnames[2] = paste("Y:", dnames[2])
+  
+  bedXY = table(Y, X, dnn = c("", "h(X|Y)")) / as.numeric(table(Y))
+  bedYX = table(X, Y, dnn = c("", "h(Y|X)")) / as.numeric(table(X))
+  
+  chi = quad_Kontingenz(X, Y)
+  m = min(nrow(bedXY), ncol(bedXY))
+  cramer = sqrt(chi / (n * (m - 1)))
+  kPearson = sqrt(chi / (chi + n)) * sqrt(m / (m - 1))
+  
+  if(all(Merkmal == "ordinal")){
+    return(list(Korrelationstabelle = table(X, Y, dnn = dnames) / normer,
+                bedingte_Verteilung_X = bedXY,
+                bedingte_Verteilung_Y = bedYX,
+                Kontingenzmass = c(Cramers_Kontingenzmass = cramer, 
+                                   korr_Pearsons_Kontingezmass = kPearson),
+                Intervall = c(unabhaengig = 0, abhaengig = 1)))
+  }
+  else{
+    return(list(Kontingenztafel = table(X, Y, dnn = dnames) / normer,
+                bedingte_Verteilung_X = bedXY,
+                bedingte_Verteilung_Y = bedYX,
+                Kontingenzmass = c(Cramers_Kontingenzmass = cramer, 
+                                   korr_Pearsons_Kontingezmass = kPearson),
+                Intervall = c(unabhaengig = 0, abhaengig = 1)))
+  }
+}
+
+
 # todo (d)
 
 # todo (e)
 
 # todo (f)
+
+# Zusatz
+
--- Funktionen-R-Skript 2.R
+++ Funktionen-R-Skript 2.R
@@ -1,2 +1,61 @@
 # Hier Helferfunktionen implementieren, welche nur in
 # Funktionen-R-Skript 1 benutzt werden duerfen.
+
+
+#quad_Kontingenz
+#
+# Funktion: berechnet die quadratische Kontingenz Chi quadrat auf zwei Arten
+# 
+# input:  x ______ ein Vektor/ein Faktor mit Merkmalsauspraegungen oder eine 
+#                  Tabelle/Matrix
+#         y ______ wenn x ein Vektor/Faktor ist, dann ein Vektor/Faktor mit den 
+#                  Auspraegungen des zweiten Merkmals
+#         method _ entweder 1 oder 2. 1) allgemeine Vorgehensweise ueber 
+#                                        Fehlerquadrate
+#                                     2) umgeformte Formel (ueber die mittlere 
+#                                        quadratische Kontingenz/Phi-Koeff.^2).
+#                  Beide Methoden liefern analytisch das selbe Ergebnis, koennen 
+#                  aber numerische Unterschiede aufweisen.
+#
+# output: die quadratische Kontingenz
+#
+quad_Kontingenz = function(x, y = NULL, method = 1){
+  if(!is.table(x) & !is.matrix(x) & is.null(y)){
+    stop("Zwei Vektoren muessen angegeben werden, alternativ eine Tabelle oder",
+         " eine Matrix.")
+  }
+  stopifnot(method %in% 1:2,
+            is.null(y) || length(x) == length(y))
+  
+  if(!is.null(dim(x))){
+    vals = as.numeric(x)
+    n = sum(x)
+    k = nrow(x)
+    l = ncol(x)
+    Hk = rowSums(x)
+    Hl = colSums(x)
+  }
+  else{
+    tab <- table(x, y)
+    vals = as.numeric(tab)
+    n = length(x)
+    k = nrow(tab)
+    l = ncol(tab)
+    Hk = rowSums(tab)
+    Hl = colSums(tab)
+  }
+  Hkl = matrix(Hk) %*% matrix(Hl, nrow = 1)
+  Hkl = as.numeric(Hkl)
+  
+  if(method == 1){
+    Hkl = Hkl / n
+    error = ((vals - Hkl)^2) / Hkl
+    error = sum(error)
+  }
+  else{
+    error = (vals^2) / Hkl
+    error = (sum(error) - 1) * n
+  }
+  
+  return(chi_quad = error)
+}

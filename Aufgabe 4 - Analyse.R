2a7d84ffc62b90d4c62723df777d5dc8561a362d
Hanjin99 <qq784365536@gmail.com>
2022-02-21 16:58:44 +0100

    Erstelle eine neue R Datei für die Aufgabe 4
    Eine neue R Datei (Aufgabe 4 - Analyse.R) wird erstellt.

---
+++ Aufgabe 4 - Analyse.R
@@ -0,0 +1,7 @@
+# Aufgabe 4
+
+# Hier sollen wir den Datensatz aus (2.) mit Hilfe der in (3.) erstellen 
+# Funktionen analysieren. (Deskription und Visualisierung)
+# Jede der Funktionen (a) bis (f) aus Funktionen-R-Skript 1.R sollen mindestens
+# einmal angewendet werden.
+

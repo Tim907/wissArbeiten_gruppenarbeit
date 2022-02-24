d5534dd568c204262a25b873e599675e9ca7ce3e
Jan-Enno-Jonas <92976451+Jan-Enno-Jonas@users.noreply.github.com>
2022-02-24 11:39:04 +0100

    Update Aufgabe 4 - Analyse.R

--- Aufgabe 4 - Analyse.R
+++ Aufgabe 4 - Analyse.R
@@ -22,7 +22,7 @@ metrischeVariablen(df$Interesse.an.Mathematik)
 #im Durchschnitt liegt das Interesse bei etwa 3.9
 
 metrischeVariablen(df$Interesse.an.Programmieren)
-# im Durchschnitt liegt das Interesse bei etwa 4.6,also hÃ¶her als das Interessant an mathematik
+# im Durchschnitt liegt das Interesse bei etwa 4.6,also hoeher als das Interessant an mathematik
 
 cor_metric_dicho(df$Interesse.an.Mathematik, df$Mathe.LK)
 # Es gibt einen leicht positiven Zusammenhang zwischen Interesse an Mathematik und Mathe LK
@@ -33,3 +33,15 @@ cor_metric_dicho(df$Interesse.an.Programmieren, df$Mathe.LK)
 
 kategorialeVariablen(df$Studienfach, Merkmal = qualitativ)
 # funktioniert nicht?
+
+Visualisierung(df)
+# Hier erkennen wir, dass in der Grafik mit den Studienfaechern das Studienfach 
+# Statistik am Häufigsten angegeben worden ist, dicht gefolgt von Data Science,deutlich
+# geringer ist das Studienfach Mathe vertreten. In der rechten, oberen Grafik 
+# können wir ablesen, dass die meisten eine Merkmalsauspraegung vo 5 angeben 
+# haben und die wenigsten ein Interesse mit 1 oder 7 angegeben haben. Bei der 
+# untereren linken Grafik sehen wir das die meisten ein Interesse von 5 bei der 
+# Programmierung haben und deutlich mehr Studierende ein höheres Interesse an
+# Programmierung haben, als an Mathe (vergleich 2 mit 3 Grafik).
+# Bei der unteren rechten Grafik können wir erkennen das deutlich mehr als die 
+# haelfte der Studierenden den Mathe LK besucht haben.
